fc6e0c158cca08ac1de103bc661d4f9b2d362337
timjolson <timjolson@user.noreplay.github.com>
2017-02-25 02:03:40 -0600

    added structs that were made previously for c++

--- PicusPy/picusData.py
+++ PicusPy/picusData.py
@@ -34,3 +34,153 @@ class Martius:
     serverConn24 = ('localhost', 6001)  # this is the controller @2.4GHz
     localTimeout = 0.02  # socket timeout
 
+#Header to identify packet type
+class HeaderType(AutoNumber):
+	ToArm = ()
+	ToRover = ()
+	FromArm = ()
+	FromRover = ()
+	Command = ()
+	Photo = ()
+	HighRes = ()
+
+#Which command you're sending (when you do)
+class CommandType(AutoNumber):
+	RecordGPS = ()
+	AddWaypoint = ()
+	TakePhoto = ()
+	DriveSoftStop = ()
+	DriveHardStop = ()
+	DriveSpeed = ()
+	DriveAngle = ()
+	ArmSoftStop = ()
+	ArmHardStop = ()
+	ArmGrab = ()
+	ArmRelease = ()
+	ArmStow = ()
+	ArmUnstow = ()
+
+#Status variables
+class StatusStruct():
+	enabled = False			#if subsystem is enabled
+	EStop = False				#if EStop is hit
+	calibrated = [0,0,0]		    #if subsystem is calibrated (may need to be array)
+	powerConnected = 0	    #changing power source, etc.
+	calibrating = 0	    #calibrating step (may need to be array)
+	communicating = 0  #millis between good packet reads
+
+
+#Controller/joystick data
+class ControlStruct():
+	x = 0	#4 proportional axes
+	y = 0
+	z = 0
+	w = 0
+	b = [0,0,0,0]	#4 buttons
+
+
+#~ #GPS coordinates and compass rotation
+#~ class GPSStruct():
+	#~ float lat
+	#~ float lon
+    #~ float alt
+    #~ float compass
+#~
+
+#~ #Absolute Orientation from IMU
+#~ class IMUStruct():
+	#~ float roll
+	#~ float pitch
+	#~ float yaw
+#~
+
+#~ #Sensor values, can roll all sensors into the one struct
+#~ class SensorStruct():
+	#~ float[2] fVal
+	#~ int[2] intVal
+#~
+
+#~ #Stores Cartesian coordinates of each joint, plus rotation and tilt of wrist
+#~ class ArmPositionStruct():
+	#~ float x	#left\right
+	#~ float y	#in\out
+	#~ float z	#up\down
+	#~ float w	#wrist angle
+	#~ float r	#wrist rotation
+#~
+
+#~ #Stores encoder values for each motor with min and max
+#~ class ArmRawStruct():
+	#~ float[6] motor #current motor position
+	#~ float[6] min	#min motor value (bottom of range)
+	#~ float[6] max	#max motor value (top of range)
+#~
+
+#~ #Packet sent to arm
+#~ class ToArmPacket():
+	#~ StatusStruct status
+	#~ ArmPositionStruct goal
+	#~ uint8_t grip
+#~
+
+#~ #Packet coming from arm
+#~ class FromArmPacket():
+	#~ StatusStruct status
+	#~ uint16_t error
+	#~ ArmPositionStruct position
+	#~ ArmRawStruct raw
+#~
+
+#~ #Arm axis PID values
+#~ class ArmPIDVals():
+	#~ float[7] kP
+	#~ float[7] kI
+	#~ float[7] kD
+#~
+
+#Packet to ROVER
+class ToRoverPacket():
+	status = StatusStruct
+	operateMode = PicusMode.driving
+	stick = ControlStruct
+	#aux = ControlStruct
+
+
+#Packet from ROVER
+class FromRoverPacket():
+	status = StatusStruct
+	#position = GPSStruct
+	wheelAngle = 0
+	wheelSpeed = 0
+	#imu = IMUStruct
+	error = 0
+
+
+#~ #Rover things PID values
+#~ class RoverPIDVals():
+	#~ float[8] kP
+	#~ float[8] kI
+	#~ float[8] kD
+#~
+
+#~ #Structure that holds a picture, with geolocation and time data
+#~ class PhotoStruct():
+	#~ uint16_t[2] size
+    #~ bool* dat
+	#~ sometype timestamp
+	#~ GPSStruct location
+	#~ IMUStruct imu
+#~
+
+#~ #Structure for high resolution camera stuff
+#~ class HighResStruct():
+	#~ int settingsorsomething
+	#~ PhotoStruct photo
+#~
+
+#~ #Struct to send commands to ROVER or Arm
+#~ class CommandStruct():
+	#~ uint16_t cmdNum
+	#~ float fVal
+	#~ int intVal
+#~
