8123708a22b6ef2daefd4a7ce719a922950cdbc0
timjolson <timjolson@user.noreplay.github.com>
2017-02-28 16:49:02 -0600

    local send/receive over tcp between scripts

---
+++ PicusPy/tcp_rx.py
@@ -0,0 +1,137 @@
+import socket, sys, time
+from picusData import *
+import pickle
+
+
+isMain = False
+
+def start_local_receive(port):
+    # start server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # set socket for quick re-use
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # bind the server address to the server object
+    server.bind((Picus.conn24[0], port))
+    # set timeout on the sockets fairly low so things don't get stuck
+    server.settimeout(Picus.localTimeout)
+
+    return server
+
+def local_receive(server, tried=False):
+    connGood = False
+    yes_return = False
+    server.listen(1)
+
+    try:
+        connection, addr = server.accept()
+        connGood = True
+        # connection is good, bump to next loop
+    except socket.timeout:
+        # connection timedout, stay in this loop
+        tried = True
+    except socket.error as err:
+        # connection error, print and stay in this loop
+        print(err)
+
+    if connGood:
+        try:
+            rx = connection.recv(1024)
+            # try to receive a packet
+        except socket.error as err:
+            # if there is an error involving client socket closing
+            if err.errno == 10054 or err.errno == 54 or err.errno == 10061:
+                print("client lost")
+
+    # process packet
+    if connGood:
+        if rx:
+            # have received data and rx != 0
+            if type(rx) == bytes:
+                rx = pickle.loads(rx)
+                yes_return = True
+            else:
+                print("RX type invalid ", rx)
+
+        else:
+            # rx == 0 means client sent 0 as disconnect
+            print("client closing")
+
+    try:
+        connection.close()
+    except: pass
+
+    if yes_return: return rx
+    else: return None
+
+# # receives and processes socket packets
+# def receiver(data=""):
+#     # start server
+#     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     # set socket for quick re-use
+#     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#
+#     # bind the server address to the server object
+#     server.bind((Martius.conn24[0], 8000))
+#     # set timeout on the sockets fairly low so things don't get stuck
+#     server.settimeout(Picus.localTimeout)
+#     # tell the server to listen for a certain number of connections
+#     server.listen(1)
+#
+#     # run this all the time
+#     while True:
+#         if isMain:
+#             print("waiting for transmitter")
+#         # wait for a connection
+#         while True:
+#             try:
+#                 connection, addr = server.accept()
+#                 # connection is good, bump to next loop
+#                 break
+#             except socket.timeout:
+#                 # connection timedout, stay in this loop
+#                 pass
+#             except socket.error as err:
+#                 # connection error, print and stay in this loop
+#                 print (err)
+#
+#         # wait for data
+#         while True:
+#             try:
+#                 rx = connection.recv(1024)
+#                 # try to receive a packet
+#             except socket.error as err:
+#                 # if there is an error involving client socket closing
+#                 if err.errno == 10054 or err.errno == 54 or err.errno == 10061:
+#                     print("client lost")
+#                     break
+#
+#             # process packet
+#             if rx:
+#                 # have received data and rx != 0
+#                 if type(rx) == bytes:
+#                     rx = pickle.loads(rx)
+#
+#                 if isMain:
+#                     if type(rx) == str:
+#                         print("RX: ", rx)
+#                     elif type(rx) == float:
+#                         print("RX: ", time.time() - rx) # print transmit and process time
+#                 # connection.sendall("rx".encode('utf-8')) # send an acknowledge if you want to
+#             else:
+#                 # rx == 0 means client sent 0 as disconnect
+#                 print("client closing")
+#                 connection.close()
+#                 break
+
+
+# starts process clock
+# starts receiver
+if __name__ == "__main__":
+    isMain = True
+    serv = start_local_receive(9000) # setup port
+
+    # watch for data on setup port
+    while True:
+        a = local_receive(serv)
+        if a: print(a)
---
+++ PicusPy/tcp_tx.py
@@ -0,0 +1,120 @@
+import socket, sys, time
+from picusData import *
+import pickle
+
+# flag for when the script is run directly
+isMain = False
+
+def local_send(port, data="",tried=True):
+    connGood = False
+    # make client socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # set socket for quick re-use
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    sock.settimeout(Picus.localTimeout)
+
+    try:
+        sock.connect((Picus.conn24[0], port))
+        connGood = True
+    except socket.error as err:
+        if err.errno == 10054 or err.errno == 54 or err.errno == 10061 or err.errno == 111:
+            pass
+        else:
+            # socket err, print and stay in this loop
+            print(err)
+
+    if connGood:
+        try:
+            sock.send(pickle.dumps(data,2))
+        # socket has an error
+        except socket.error as err:
+            # error involving server close
+            if err.errno == 10054 or err.errno == 54 or err.errno == 10061:
+                if isMain: print("no receiver")
+                try:
+                    # try to close the socket so we can restart it
+                    sock.close()
+                except socket.error:
+                    pass  # if socket did not exist on .close(), ignore error
+    else:
+        if not tried:
+            sendTCP(port, data, tried=1)
+        else:
+            if isMain: print("no receiver")
+            pass
+
+# # send data
+# def sender(data=""):
+#     # flag for presence of connection
+#     connGood = False
+#
+#     # do this all the time
+#     while True:
+#         # make client socket
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         # set socket for quick re-use
+#         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#
+#         # wait for connection
+#         print("waiting for receiver")
+#         while True:
+#             try:
+#                 sock.connect((Martius.conn24[0], 8000))
+#                 # try to connect to server
+#                 connGood = True
+#                 # connection good if made it this far
+#                 break
+#             except socket.error as err:
+#                 connGood = False
+#                 if err.errno == 10054 or err.errno == 54 or err.errno == 10061:
+#                     pass
+#                 else:
+#                     # socket err, print and stay in this loop
+#                     print(err)
+#
+#         # until connection has problem
+#         while connGood:
+#             # running directly, pause between tx
+#             # if isMain:
+#             #     time.sleep(1.5)
+#
+#             # try sending data
+#             try:
+#                 # running directly, print what was transmitted
+#                 if isMain and data=="":
+#                     print("TX: ", time.time())
+#                     sock.send(pickle.dumps(time.time()))
+#                 elif isMain:
+#                     print("TX: ", data)
+#                     sock.send(pickle.dumps(data))
+#                 else:
+#                     sock.send(pickle.dumps(data))
+#
+#             # socket has an error
+#             except socket.error as err:
+#                 # error involving server close
+#                 if err.errno == 10054 or err.errno == 54 or err.errno == 10061:
+#                     print("lost receiver")
+#                     try:
+#                         # try to close the socket so we can restart it
+#                         sock.close()
+#                     except socket.error: pass # if socket did not exist on .close(), ignore error
+#                     connGood = False # flag connection has ended
+#                     break  # return to first loop
+#
+#                 pass # made it this far, stay in this loop
+#
+#             # between restart, delay a while
+#             time.sleep(0.01)
+
+
+# sets flag, starts process clock, sends data
+if __name__ == "__main__":
+    isMain = True
+
+    # send data over port
+    data = 123 # can be text or whatever
+    while True:
+        local_send(9000, data)
+        time.sleep(0.5)
