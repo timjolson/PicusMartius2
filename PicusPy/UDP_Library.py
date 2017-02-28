ca0a03d733b0a40d0ec3d131bae8612cf7f8932a
timjolson <timjolson@user.noreplay.github.com>
2017-02-27 23:14:49 -0600

    udp send and receive library update

--- PicusPy/UDP_Library.py
+++ PicusPy/UDP_Library.py
@@ -4,10 +4,9 @@ import socket
 from socket import *
 import time
 from picusData import *
+import pickle
 
 class UDP_Library():
-    freq = 20
-    last = 0
 
     def __init__(self, myAddr, address):
         self.address = address
@@ -17,52 +16,65 @@ class UDP_Library():
             self.client_socket.bind(myAddr) # this is me
         except error as err:
             if err.errno == 10049:
-                #print("\r\nAddress error, try setting machine static IP or updating myAddr in the script...\r\n")
                 raise Exception("\r\r\n\nAddress error, try setting machine static IP or updating myAddr in the script...")
 
-        self.client_socket.settimeout(0.1) #Only wait .1 second for a response
+        self.client_socket.settimeout(Picus.localTimeout) #Only wait .1 second for a response
 
-    def sendUDP(self, message):
+    def sendDAT(self, message):
+        try:
+            self.client_socket.sendto(message, self.address)
+        except:
+            print("send dat fail")
+            pass
+
+    def sendTXT(self, message):
         try:
             self.client_socket.sendto(message.encode('utf-8'), self.address)
         except:
-            #TODO: print when no connection
+            print("send text fail")
             pass
 
     def receiveUDP(self):
-        if time.clock() - self.last > (1/self.freq):
-            self.last = time.clock()
-            try:
-                #print("b4")
-                rxText, addr = self.client_socket.recvfrom(1024)
-                print("rx:"+rxText.decode('utf-8'))
-                return rxText.decode('utf-8')
-            except timeout:
-                print("timeout")
-                return 0
-            except herror:
-                print("herror")
-                return 0
-            except gaierror:
-                print("gaierror")
-                return 0
-            except error as msg:
-                print("error:", msg)
-                return 0
-        else:
-            #self.last = time.clock()
+        try:
+            rx, addr = self.client_socket.recvfrom(1024)
+            return rx
+        except timeout:
+            #print("timeout")
+            return 0
+        except herror:
+            print("herror")
+            return 0
+        except gaierror:
+            print("gaierror")
+            return 0
+        except error as msg:
+            print("error:", msg)
             return 0
 
 
 if __name__ == '__main__':
-    myAddr = ("192.168.1.100", 5000)
-    otherAddr = ("192.168.1.200", 5000)
+    start = time.time()
+    last = 0
+    myAddr = ("192.168.1.200", 7008)
+    otherAddr = ("192.168.1.100", 6008)
     otherside = UDP_Library( myAddr, otherAddr)
     while True:
-        otherside.sendUDP("test1234test9876")
-        print("tx")
-        time.sleep(0.5)
+        #otherside.sendTXT("t")
+        curr = time.time()
+        if curr - last > 3:
+            last = curr
+            print("waiting")
+
         try:
-            print(otherside.receiveUDP())
+            rx = otherside.receiveUDP()
+            if rx:
+                last = curr
+                rx = pickle.loads(rx)
+                print(rx.x)
         except:
+            #print("no pickle")
             pass
+
+        if curr - start > 30:
+            print("time up")
+            break
