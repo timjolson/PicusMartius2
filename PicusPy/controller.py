0eed48a506f9bb908ed5a160270f6591cdfc8a76
timjolson <timjolson@user.noreplay.github.com>
2017-05-02 01:09:51 -0500

    changes to controller prints and data rate

--- PicusPy/controller.py
+++ PicusPy/controller.py
@@ -1,11 +1,12 @@
-import pygame, sys, time # only used for the examples at the bottom
+import pygame, sys, time, os  # only used for the examples at the bottom
 from picusData import *
 from joystick_Library import *
 from UDP_Library import *
+from numpy import mod
 
 myAddr = Martius.conn24
 roverAddr = Picus.conn24
-freq = 20
+freq = 10
 
 j = joystick_Library(0)
 j.start_Joystick()
@@ -15,18 +16,20 @@ rover = UDP_Library( myAddr, roverAddr )
 
 while(True):
     curr = time.time()
+
     if curr - last > 1/freq:
         last = curr
         pygame.event.pump()
         tx = j.read_Joystick()
         if tx:
+            if abs(tx.y) < 0.024: tx.y = 0.0
             rover.sendDAT(pickle.dumps(tx))
-        print(tx.x)
+            print(tx.y)
 
     try:
         rx = 0
         rx = rover.receiveUDP()
-        if rx!=0: print(rx)
+        if rx!=0: print('rx:%a' %(rx))
     except:
         pass
 
