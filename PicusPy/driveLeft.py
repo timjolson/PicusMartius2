e6d035bae0b5c4538469797c498a5908f5883d12
timjolson <timjolson@user.noreplay.github.com>
2017-05-02 01:09:31 -0500

    right and left driving scripts

--- PicusPy/driveLeft.py
+++ PicusPy/driveLeft.py
@@ -1,24 +1,28 @@
-import wiringpi2 as droid
+# import wiringpi2 as droid
 import time, sys
 from tcp_rx import *
 from tcp_tx import *
 from picusData import *
 
-lp = Picus.local_ports
+def pinWrite(pin, val):
+	local_send(Picus.local_ports['M'], bytes(pickle.dumps((pin, val))))
+
+manager = start_local_receive(Picus.local_ports['L'])
 
 start = time.time()
 last = start
 
-droid.wiringPiSetupGpio()
+# droid.wiringPiSetupGpio()
 
-wheel = [19, 30, 29]
+wheel = [5, 6, 7]
+# wheel = [5]
 
 for w in wheel:
-    droid.pinMode(w, 1)
-    droid.softPwmCreate(w, 0, PWM.freqRange)
-    droid.softPwmWrite(w, PWM.neutral)
-
-manager = start_local_receive(lp['L'])
+    # droid.pinMode(w, 1)
+    # droid.softPwmCreate(w, 0, PWM.freqRange)
+    # droid.softPwmWrite(w, PWM.neutral)
+    pinWrite(w, PWM.neutral)
+    time.sleep(0.01)
 
 print("Left Drive Started")
 
@@ -29,21 +33,26 @@ while True:
         last = curr
         if type(rx) == float:
             for w in wheel:
-                droid.softPwmWrite(w, PWM.neutral + int(rx*PWM.range))
+                 # droid.softPwmWrite(w, PWM.neutral + int(rx*PWM.range))
+                pinWrite(w, PWM.neutral + int(rx*PWM.range))
+                time.sleep(0.01)
         else:
             print("Left Drive stopped")
             for w in wheel:
-                droid.softPwmWrite(w, PWM.neutral)
+                # droid.softPwmWrite(w, PWM.neutral)
+                pinWrite(w, PWM.neutral)
+                time.sleep(0.01)
             break
 
     if (curr - last) > Picus.driveTimeout:
         for w in wheel:
-            droid.softPwmWrite(w, PWM.neutral)
-
-    if curr - last > 10:
-        break
+            pinWrite(w, PWM.neutral)
+            time.sleep(0.01)
+            #droid.softPwmWrite(w, PWM.neutral)
 
 for w in wheel:
-    droid.softPwmWrite(w, PWM.neutral)
+    pinWrite(w, PWM.neutral)
+    time.sleep(0.01)
+    # droid.softPwmWrite(w, PWM.neutral)
 
 exit(0)
--- PicusPy/driveRight.py
+++ PicusPy/driveRight.py
@@ -1,24 +1,28 @@
-import wiringpi2 as droid
+# import wiringpi2 as droid
 import time, sys
 from tcp_rx import *
 from tcp_tx import *
 from picusData import *
 
-lp = Picus.local_ports
+def pinWrite(pin, val):
+	local_send(Picus.local_ports['M'], bytes(pickle.dumps((pin, val))))
+
+manager = start_local_receive(Picus.local_ports['R'])
 
 start = time.time()
 last = start
 
-droid.wiringPiSetupGpio()
+# droid.wiringPiSetupGpio()
 
-wheel = [21, 18, 22]
+wheel = [2, 3, 12]
+# wheel = [2]
 
 for w in wheel:
-    droid.pinMode(w, 1)
-    droid.softPwmCreate(w, 0, PWM.freqRange)
-    droid.softPwmWrite(w, PWM.neutral)
-
-manager = start_local_receive(lp['R'])
+    pinWrite(w, PWM.neutral)
+    time.sleep(0.01)
+    # droid.pinMode(w, 1)
+    # droid.softPwmCreate(w, 0, PWM.freqRange)
+    # droid.softPwmWrite(w, PWM.neutral)
 
 print("Right Drive Started")
 
@@ -29,21 +33,27 @@ while True:
         last = curr
         if type(rx) == float:
             for w in wheel:
-                droid.softPwmWrite(w, PWM.neutral + int(rx*PWM.range))
+                pinWrite(w, PWM.neutral + int(rx*PWM.range))
+                time.sleep(0.01)
+                #print(PWM.neutral + int(rx*PWM.range))
+                # droid.softPwmWrite(w, PWM.neutral + int(rx*PWM.range))
         else:
             print("Right Drive stopped")
             for w in wheel:
-                droid.softPwmWrite(w, PWM.neutral)
+                pinWrite(w, PWM.neutral)
+                time.sleep(0.01)
+                # droid.softPwmWrite(w, PWM.neutral)
             break
 
     if (curr - last) > Picus.driveTimeout:
         for w in wheel:
-            droid.softPwmWrite(w, PWM.neutral)
-
-    if curr - last > 10:
-        break
+            pinWrite(w, PWM.neutral)
+            time.sleep(0.01)
+            # droid.softPwmWrite(w, PWM.neutral)
 
 for w in wheel:
-    droid.softPwmWrite(w, PWM.neutral)
+    pinWrite(w, PWM.neutral)
+    time.sleep(0.01)
+    # droid.softPwmWrite(w, PWM.neutral)
 
 exit(0)
