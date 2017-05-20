4516ca0b2d06824b79c1b78737e89f1cadd17f26
timjolson <timjolson@user.noreplay.github.com>
2017-05-20 13:11:19 -0500

    comments

--- PicusPy/mega.py
+++ PicusPy/mega.py
@@ -1,41 +1,63 @@
+# This script was setup to manage receiving TCP data and sending to an arduino/serial object.
+# This was integrated directly into the main script, but a class to do this would be beneficial.
+# In the case of a class, TCP could be skipped for a method.
+
 import serial, time, sys
 from tcp_rx import *
 from tcp_tx import *
 from picusData import *
 import pickle
 
+# TCP receiver
 manager = start_local_receive(Picus.local_ports['M'])
 
+# time tracking
 start = time.time()
 last = start
 
+# connect to serial object
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.01)
+# arduino takes a while to wake up
 time.sleep(1)
+
 print("MEGA Started")
 
-
+# do things
 while True:
+    # update time
     curr = time.time()
-    rx = local_receive(manager)
-    if rx:
-        last = curr
-        rx = pickle.loads(bytes(rx, 'utf-8'))
-        if type(rx) is tuple:
-            print('%d,%d' % (rx[0], rx[1]))
-            # if ser.isOpen():
-            ser.write(bytes('&&', 'utf-8')+bytes(str(rx[0]), 'ascii')+bytes(',', 'utf-8')+bytes(str(rx[1]), 'ascii')+bytes(',', 'utf-8'))
-            time.sleep(0.005)
-            # else:
-            #     try:
-            #         ser.open()
-            #     except:
-            #         pass
-            # print(ser.readline())
 
+    # get TCP data
+    rx = local_receive(manager)
+
+    # if we have TCP data
+    if rx:
+        # track time
+        last = curr
+
+        # unpack TCP data
+        rx = pickle.loads(bytes(rx, 'utf-8'))
+
+        # if it's a tuple (that's how we send the stuff to this script)
+        if type(rx) is tuple:
+            # print the two values
+            # values are (pin number, PWM write value)
+            print('%d,%d' % (rx[0], rx[1]))
+
+            # format and write the data
+            ser.write(bytes('&&', 'utf-8')+bytes(str(rx[0]), 'ascii')+bytes(',', 'utf-8')+bytes(str(rx[1]), 'ascii')+bytes(',', 'utf-8'))
+
+            # wait
+            time.sleep(0.005)
+
+    # if we have not received any TCP in 3 seconds, stop script
     if (curr - last) > 3:
         break
 
+# close serial connection
 ser.close()
+
+# delete serial object
 del ser
 
 print("mega stopped")
--- PicusPy/myplot.py
+++ PicusPy/myplot.py
@@ -1,73 +1,122 @@
 # myplot class takes datastream objects and
-# interactively plots them with pyplot
-# display options are set in constructor
+# interactively plots them with pyplot.
+# Display options are set in constructor.
+
+# To better understand the options, play with the example
+
 from datastream import datastream
 from matplotlib import pyplot as plt
 
 class myplot:
 
+    # class attribute so you can have multiple plots at once (TODO:test that)
     fignum = 1
 
+    # constructor
+    # arguments:
+    # title
+    # x label
+    # y label
+    # fixed - if vertical limits are fixed or dynamically adjust to min/max of data
+    #         (limits will always adjust if data is off-screen)
+    # timespan - duration of data to show
+    # grid - whether to show grid on plot
+    # show - whether to actually plot the data (object used for storage otherwise)
+    # showMinMax - draw lines at min and max data values
+    # legend - show legend
+    # setY - manually set y limits (pass in list)
     def __init__(self, title='', xlab='', ylab='', fixed=True, timespan=5, grid=False, show=False,
                  showMinMax=False, legend=False, setY=None):
+        # options
         self.fixed = fixed
-        self.streams = []
         self.timespan = timespan
         self.title = title
         self.xlabel = xlab
         self.ylabel = ylab
+        self.grid = grid
+        self.showMM = showMinMax
+        self.legend = legend
+
+        # list of
         self.ls = []
+        # list of datastreams
+        self.streams = []
+
+        # y value of min and max lines
         self.markYmin = 0
         self.markYmax = 0
+
+        # y value of display min and max
         self.permYmax = 0
         self.permYmin = 0
+
+        # min and max x and y values for the figure
         self.minx = 0
         self.maxx = 0
         self.miny = 0
         self.maxy = 0
-        self.grid = grid
+
+        # keep my figure number
         self.myfignum = self.fignum
         self.fignum += 1
-        self.inited = False
-        self.legend = legend
+
+        # by default, limits are not manually set
         self.setLims = False
-        self.showMM = showMinMax
+
+        # manual y limits
         if setY: self.setY(setY[0], setY[1])
+
+        # if we show the plot, it needs to be initialized for that
+        self.inited = False
+        # show plot
         if show: self.initShow()
 
+    # set y limits manually
     def setY(self, a, b):
         self.setMinY = min(a,b)
         self.setMaxY = max(a,b)
         self.setLims = True
 
+    # set  title and labels
     def setLabels(self, title, xlab, ylab):
         self.title = title
         self.xlabel = xlab
         self.ylabel = ylab
 
+    # set title
     def setTitle(self, title):
         self.title = title
 
+    # set x label
     def setXLabel(self, lab):
         self.xlabel = lab
 
+    # set y label
     def setYLabel(self, lab):
         self.ylabel = lab
 
+    # add datastream object to the plot
     def addStream(self, strm_):
         self.streams.append(strm_)
 
+    # update data stream at an index with x and y values
+    # pass in as tuple or two values
     def updateStream(self, idx, x, y=None):
         if y is None and type(x) is tuple:
             self.streams[idx].add(x)
         else:
             self.streams[idx].add(x, y)
 
+    # get the streams after updating and processing them
     def getStreams(self):
+        # list of datastream points
         self.ls = []
 
+        # set plot min and max's
         self.minx, self.maxx, self.miny, self.maxy = 0, 0, 0, 0
 
+        # update each stream, track largest and smallest values across all
+        # add data to (ls)
         for stream in self.streams:
             stream.update(self.timespan)
             self.minx = max(stream.minx, self.minx)
@@ -76,61 +125,97 @@ class myplot:
             self.maxy = max(stream.maxy, self.maxy)
             self.ls.append(stream)
 
+        # if limits are automatic
         if self.setLims is False:
+            # set display limits by largest/smallest ever data
             self.permYmin = min(self.miny, self.permYmin)
             self.permYmax = max(self.maxy, self.permYmax)
 
+            # if display limits are fixed
             if self.fixed:
+                # return data, plot limits, min and max y's
                 return self.ls, (self.minx, self.maxx, self.permYmin, self.permYmax), (self.miny, self.maxy)
             else:
+                # return data, plot limits, min and max y's
                 return self.ls, (self.minx, self.maxx, self.miny, self.maxy), (self.miny, self.maxy)
+        # limits are manual
         else:
+            # return data, plot limits, min and max y's
             return self.ls, (self.minx, self.maxx, self.setMinY, self.setMaxY), (self.miny, self.maxy)
 
+    # use loop() as condition in a while() to:
+    # 1. clear the plot each call
+    # 2. break the while loop when plot is closed
     def loop(self):
         open = self.isOpen()
         if open: self.clear()
         return open
 
+    # return if plot is open
     def isOpen(self):
         return plt.fignum_exists(self.myfignum)
 
+    # clear the plot
     def clear(self):
         try:
             plt.clf()
         except:
             pass
 
+    # set up to actually show the plot
     def initShow(self):
-            self.fig = plt.figure()
-            self.myfignum = plt.gcf().number
-            plt.autoscale(False)
-            plt.ion()  # set interactive plotting
-            self.inited = True
+        # set current figure
+        self.fig = plt.figure()
+        # get and store the figure number for later
+        self.myfignum = plt.gcf().number
+        # disable autoscale
+        plt.autoscale(False)
+        # set plotting interactive (updates as it goes)
+        plt.ion()  # set interactive plotting
+        # we have been initialized for showing plot
+        self.inited = True
 
+    # show the plotted data
     def show(self):
+        # get all updated and processed stream info
         substreams, lims, ylines = self.getStreams()
 
+        # set control to the relevant figure, not a rando one
         plt.figure(self.myfignum)
+
+        # plot data with legend labels
         if self.legend:
             for STR in substreams:
                 plt.plot(STR.X, STR.Y, STR.style, label=STR.label)
+        # plot data without legend lables
         else:
             for STR in substreams:
                 plt.plot(STR.X, STR.Y, STR.style)
 
+        # set axis limits
         plt.axis(lims)
+
+        # display min/max y value lines
         if self.showMM:
             plt.plot([lims[0], lims[1]], [ylines[0], ylines[0]], 'k--')
             plt.plot([lims[0], lims[1]], [ylines[1], ylines[1]], 'k--')
+
+        # show grid
         if self.grid: plt.grid(True)
+
+        # show title and axis labels
         plt.title(self.title)
         plt.xlabel(self.xlabel)
         plt.ylabel(self.ylabel)
+
+        # anchor legend top left
         if self.legend: plt.legend(bbox_to_anchor=(0, 1), loc=2, borderaxespad=0.)
+
+        # actually display plot (pass in time to pause the data)
         plt.pause(0.001)
 
 
+# example
 if __name__ == "__main__":
     import numpy as np
     import time
@@ -151,7 +236,7 @@ if __name__ == "__main__":
     # 3. all at once, outside constructor
     # dat.setLabels('title','x here', 'y here')
 
-    # create some datastreams, with plot styles
+    # create some datastreams, with plot styles and labels
     feedbackData = datastream('r', 'feedback')
     positionData = datastream('g', 'position')
     diff = datastream('k', 'error')
@@ -161,15 +246,18 @@ if __name__ == "__main__":
     dat.addStream(positionData)
     dat.addStream(diff)
 
+    # loop through showing data
+    # notice loop() as the while condition, this clears the plot and stops the loop when plot is closed
     while dat.loop():
+        # update time
         now = time.time() - start
 
-        # loop through datastreams in dat, do math to generate data
+        # loop through datastreams in dat, generate and add data
         for idx, STR in enumerate(dat.streams):
             if idx<2:
                 STR.add(now, np.exp(-.05*now)*np.sin(now + np.pi/(1+idx)))
 
-        # or add data to a stream separately
+        # or add data to a stream specifically
         err = feedbackData.Y[-1] - positionData.Y[-1]
         diff.add(now, err)
 
