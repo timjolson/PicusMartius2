7fc167b348c74a1065084e33c4bbe8b5879f1306
timjolson <timjolson@user.noreplay.github.com>
2017-05-20 12:31:11 -0500

    commenting latest working versions

--- PicusPy/controller.py
+++ PicusPy/controller.py
@@ -1,36 +1,77 @@
-import pygame, sys, time, os  # only used for the examples at the bottom
+import time
+import pygame, os
 from picusData import *
 from joystick_Library import *
 from UDP_Library import *
-from numpy import mod
 
-myAddr = Martius.conn24
-roverAddr = Picus.conn24
-freq = 10
-
+# try to initialize plugged-in controller
 j = joystick_Library(0)
+# set it up for use
 j.start_Joystick()
+
+# if this script is run on windows
+if os.name == 'nt':
+    WINDOWS = True
+    # use radio for connection
+    myAddr = Martius.conn24
+    roverAddr = Picus.conn24
+# otherwise
+else:
+    WINDOWS = False
+    # use local connection (TODO: test this again)
+    myAddr = ('localhost', Martius.conn24[1])
+    roverAddr = ('localhost', Picus.conn24[1])
+
+# this print will show your IP address or something, don't remember
+#print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+
+# set frequency of controller data sending
+freq = 10
+# keep track of last time sent
 last = time.clock()
 
+# set up communication to rover
 rover = UDP_Library( myAddr, roverAddr )
 
+
+# do all this stuff
 while(True):
+    # update time at beginning of loop
     curr = time.time()
 
+    # if sending period has elapsed
     if curr - last > 1/freq:
+        # update last send time
         last = curr
+
+        # get joystick data
         pygame.event.pump()
         tx = j.read_Joystick()
+
+        # if joystick reads properly
         if tx:
+            # apply deadband on y-axis
             if abs(tx.y) < 0.024: tx.y = 0.0
+            # make tx structure into packed string and send it to rover
             rover.sendDAT(pickle.dumps(tx))
-            print(tx.y)
 
-    try:
-        rx = 0
-        rx = rover.receiveUDP()
-        if rx!=0: print('rx:%a' %(rx))
-    except:
-        pass
+        # if button 9 is pressed
+        if tx.b9:
+            # send structure (2nd time)
+            rover.sendDAT(pickle.dumps(tx))
+            # break big while loop
+            break
 
+    # if we're running in windows
+    if WINDOWS==True:
+        try:
+            # check for received data and print it
+            rx = 0
+            rx = rover.receiveUDP()
+            if rx!=0: print('rx:%a' %(rx))
+        except:
+            # not having data is okay
+            pass
+
+# return good exit code to system (useful in bash scripts)
 exit(0)
--- PicusPy/datastream.py
+++ PicusPy/datastream.py
@@ -1,84 +1,115 @@
 # datastream class handles information for a data set:
 # x and y coordinates
-# the min and max
+# the display min and max
 # updates the stream to remove old data outside the requested timespan
-
-import numpy as np
 
 
 class datastream:
 
+    # constructor takes in optional plotting style and label for the stream(~= a line in matlab terms)
     def __init__(self, style_='k', label=''):
+        # initialize attributes
         self.minx, self.maxx, self.miny, self.maxy, self.xrange, self.yrange = 0, 0, 0, 0, 0, 0
         self.style = style_
-        self.X = []
-        self.Y = []
         self.label = label
 
+        # store x and y coordinates (each update stores both, pairs stick together)
+        self.X = []
+        self.Y = []
+
+    # add a datapoint to the stream
     def add(self, x_, y_):
         self.X.append(x_)
         self.Y.append(y_)
 
+    # update the stream to remove data older than the timepsan (ts) and change the display min and max
+    # NOTE: X is assumed to be timestamps
     def update(self, ts=10):
+        # if X is not empty
         if len(self.X)>1:
+            # X is time, so range is (last - first)
             self.xrange = self.X[-1] - self.X[0]
 
+        # when the range of data is bigger than timespan
         while self.xrange > ts:
+            # remove first data point
             self.X = self.X[1:]
             self.Y = self.Y[1:]
+            # update the time range
             self.xrange = self.X[-1] - self.X[0]
 
+        # update the min and max time
         self.minx = self.X[0]
         self.maxx = self.X[-1]
 
+        # update min and max Y values and range
         self.miny = min(self.Y)
         self.maxy = max(self.Y)
         self.yrange = self.maxy - self.miny
 
+    # get complete datastream as 2D list
     def getDataPoints(self):
         return [self.X, self.Y]
 
+    # get complete datastream as tuples
     def getDataTup(self):
         return self.X, self.Y
 
+# Example generates two datastreams, then updates them for 3 lengths.
+# Close each plot to see the next.
+# Script will continue running until all plots are closed.
 if __name__ == "__main__":
     import time
+    import numpy as np
     from matplotlib import pyplot as plt
+
+    # set elapsed time
     elapsed = 0
+
+    # make 2 datastream objects
     dat = datastream('b-.')
     dat2 = datastream('k')
 
-    # test adding x and y's
+    # test adding x and y's for 80 cycles
     while elapsed < 80:
-         dat.add(elapsed, np.exp(-0.1*elapsed)*np.sin(.5*elapsed))
-         dat2.add(elapsed, elapsed*2/(elapsed*elapsed+.1))
-         elapsed += 1
+        # add oscillating and decaying data to (dat)
+        dat.add(elapsed, np.exp(-0.1*elapsed)*np.sin(.5*elapsed))
+        # add decaying data to (dat2)
+        dat2.add(elapsed, elapsed*2/(elapsed*elapsed+.1))
+        elapsed += 1
 
-    # test methods
+    # test methods (make sure they don't break)
     dat.getDataPoints()
     dat2.getDataPoints()
     dat.getDataTup()
     dat2.getDataTup()
 
-    # plot
+    # plot last 100 datapoints (if X values were timestamps, you'd use seconds)
     dat.update(100)
     dat2.update(100)
     plt.plot(dat.X, dat.Y, dat.style)
     plt.plot(dat2.X, dat2.Y, dat2.style)
+    plt.title('100 data points')
+    # show grid
     plt.grid(True)
+    # display plot
     plt.show()
 
+    # plot last 70 datapoints
     dat.update(70)
     dat2.update(70)
     plt.plot(dat.X, dat.Y, dat.style)
     plt.plot(dat2.X, dat2.Y, dat2.style)
+    plt.title('70 data points')
     plt.grid(True)
     plt.show()
 
+    # plot last 60 datapoints
     dat.update(60)
     dat2.update(60)
     plt.plot(dat.X, dat.Y, dat.style)
     plt.plot(dat2.X, dat2.Y, dat2.style)
+    plt.title('60 data points')
     plt.grid(True)
     plt.show()
 
