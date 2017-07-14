a9b1b198f98bf477adc0b76dbdbea934dbff4c24
timjolson <timjolson@users.noreply.github.com>
2017-07-13 23:42:57 -0500

    added adjustable ramping limits

--- PicusPy/PID.py
+++ PicusPy/PID.py
@@ -9,9 +9,9 @@ import time
 
 class PID:
 
-    # constructor - pass in gains, deadband, refresh interval, smoothing option,
+    # constructor - pass in gains, deadband, refresh interval,
     # and whether to accumulate control values
-    def __init__(self, kP, kI, kD, deadband=0.0, refreshTime=0.1, smooth=True, add=True):
+    def __init__(self, kP, kI, kD, deadband=0.0, refreshTime=0.1, add=True, rampUp=0.8, rampDown=None):
         # status
         self.lastCalc = time.time()# track the last time PID was calculated
         self.enabled = False    # is controller enabled
@@ -19,8 +19,20 @@ class PID:
         self.position = 0.0     # current position of system
         self.control = 0.0      # speed/output value (scale +/- 0.0 to 1.0)
         self.error = 0.0        # current error
-        # self.derr = 0.0       # rate of error change (superseded by dpos)
+        # self.derr = 0.0      # rate of error change (superseded by dpos)
         self.dpos = 0.0         # rate of position change
+        
+        # handle ramping up
+        if rampUp == None or rampUp == 0.0: # set to no ramping limit
+            self.rampUp = 0.8            # 800 ms to full speed
+        else:   # value specified
+            self.rampUp = abs(rampUp) # max acceleration (seconds to ramp up to full speed)
+        
+        #handle ramping down
+        if rampDown is None or rampDown == 0.0: # set to no ramping limit
+            self.rampDown = 0.3              # 300 ms to full stop
+        else:   # value specified
+            self.rampDown = abs(rampDown)       # max deceleration (seconds to ramp fully down)
         
         # gains
         self.kP = kP        # proportional gain
@@ -29,19 +41,18 @@ class PID:
         
         # option variables
         self.deadband = deadband        # deadband zone size (output set to 0 if |error|<deadband
-        self.smooth = smooth            # smooth maintains the control value when setpoint is changed, otherwise it is reset to 0.0
         self.refreshTime = refreshTime  # time interval (sec) between new calculations
         self.additive = add             # whether control is cumulative or calculated from scratch each time
         
         # backend variables
         self.integral = 0.0     # store integral term
-        # self.prevError = 0.0 # system's previous error (superseded by prevPos)
+        # self.prevError = 0.0# system's previous error (superseded by prevPos)
         self.prevPos = None     # system's previous position
         self.skipDeriv = True   # whether to skip derivative calculation (first cycle after enable() 
                                 # or setpoint change could produce instability)
         
     # get pid error and control value, pass in position and optionally setpoint
-    # run this all the time and enable() and disable() the obj when needed
+    # run this all the time/short intervals and run enable() and disable() when needed
     # 
     # error, control = obj.get()
     def get(self, position, setpoint=None):
@@ -55,7 +66,7 @@ class PID:
         self.error = self.setpoint - position
 
         ## if the controller is enabled
-        if self.enabled == 1 or self.enabled is True:
+        if self.enabled is True:
             # get current time
             now = time.time()
             # time since last calculation
@@ -65,12 +76,12 @@ class PID:
             if self.refreshTime < self.dt and self.dt < self.refreshTime*3:
                 ## update integral
                 # adding right into the term removes large jumps on setpoint change
-                self.integral += self.kI * self.error
+                self.integral += self.kI * self.error * self.dt
                 
                 ## delta error
                 # self.derr = self.error - self.prevError # superseded by change 
-                # in position (dpos), which == change in error but on a physical
-                # system is smooth (no jump on setpoint change) if feedback is smooth
+                # in position (dpos), which == change in error on a real physical
+                # system with smooth feedback
                 self.dpos =  (self.prevPos if self.skipDeriv is False else 0.0) - position
                 self.dpos /= self.dt    # change in position / time = rate
                 
@@ -84,13 +95,33 @@ class PID:
                     self.integral = 0.0
                     self.control = 0.0
                     return self.error, self.control
-
+                
+                ## limit ramping up and down
+                # get ramp acceleration limit
+                RU = self.dt/self.rampUp
+                RD = self.dt/self.rampDown
+                # calculate new control value
+                new_control = self.kP * self.error + self.integral + self.kD * self.dpos
+                # if not additive control, the change from last control is acceleration
+                accel = new_control if self.additive is True or self.additive == 1 else new_control - self.control
+                
+                ## apply ramping limits
+                # when close to stopped, use rampUp either direction
+                if abs(self.control)<0.05:
+                    temp = -RU if accel < -RU else ( RU if accel > RU else new_control)
+                # when going forward, use rampDown if new_control < 0 == decelerating
+                elif self.control>0:
+                    temp = -RD if accel < -RD else ( RU if accel > RU else new_control)
+                # when going reverse, use rampDown if new_control > 0 == decelerating
+                else: # self.control<0:
+                    temp = -RU if accel < -RU else ( RD if accel > RD else new_control)
+                
                 ## set control value
                 # it's either cumulative or from scratch
                 if self.additive is True or self.additive == 1:
-                    self.control += self.kP * self.error + self.integral + self.kD * self.dpos
+                    self.control += temp
                 else:
-                    self.control = self.kP * self.error + self.integral + self.kD * self.dpos
+                    self.control = temp
                 
                 ## check for saturation and limit control to +/-1
                 self.control = -1.0 if self.control < -1.0 else ( 1.0 if self.control > 1.0 else self.control)
@@ -109,6 +140,7 @@ class PID:
             
             ## was not time to recalculate and was not long enough for an error
             ## just return the previous values
+            # return self.error, self.control
             return self.error, self.control
 
         ## controller is disabled
@@ -137,11 +169,11 @@ class PID:
     # change the calculation gains
     # pass in all three gains
     # optionally pass in whether or not to enable() the object at the same time
-    def setGains(self, kP, kI, kD, enable=0):
+    def setGains(self, kP, kI, kD, enable=False):
         self.kP = kP
         self.kI = kI
         self.kD = kD
-        if enable == 1 or enable is True:
+        if enable is True:
             self.enable()
         else:
             # if enable not desired, make sure disabled
@@ -152,15 +184,7 @@ class PID:
     def setSP(self, sp):
         # setpoint is different than the last
         if sp != self.setpoint:
-            # if we want to reset speed/output with goal change
-            if self.smooth is not True and self.smooth != 1:
-                self.control = 0.0
-                # reset integral
-                self.integral = 0.0
-                self.skipDeriv = True
-            else:
-                self.skipDeriv = False
-            
+            self.skipDeriv = True
             # update setpoint
             self.setpoint = sp
 
@@ -174,28 +198,29 @@ if __name__ == "__main__":
     timePerGoal = 4 # time between goal/setpoint changes
     gravity = 0.0   # initial gravity (changes to 0.5 after full tuning cycle)
     pos = [0]       # initial position for 'physical system'
-    goallist = [-2, 2] # list of setpoints (each gain set will use all goals)
+    goallist = [-2, 2, -2, 2] # list of setpoints (each gain set will use all goals)
     goalidx = 0     # starting setpoint in goallist
     
     # names of gain sets
-    titlelist = ['slow rise', 'underdamped', 'overshoot', 'well-tuned']
+    titlelist = ['slow rise', 'underdamped', 'overshoot=underdamped', 'well-tuned']
     # sets of gains
-    kplist = [0.02, 0.18, 0.20, 0.20]
+    kplist = [0.01, 0.20, 0.20, 0.20]
     kilist = [0.00, 0.00, 0.00, 0.00]
-    kdlist = [0.04, 0.03, 0.06, 0.09]
+    kdlist = [0.015, 0.035, 0.075, 0.091]
     gainidx = 0 # choose set of starting gains
     
-    # make datastream objects, where we store data points
+    # make datastream objects, where we store data points 
+    # data can also be directly stored inside a myplot object
     errstream = datastream('r--','error')
     pidstream = datastream('g','PID control')
     goalstream = datastream('k--','setpoint')
     positionstream = datastream('b','position')
     
     # make myplot object, this is a window/graph that scrolls with the data
-    myplotObj = myplot('PID Example', 'time', 'value', fixed=True, timespan=10,
+    myplotObj = myplot('PID Example', 'time', 'value', fixed=True, timespan=7,
             show=True, legend=True, grid=True, showMinMax=False)
     # set window y axis limits (optional)
-    myplotObj.setY(-4,8)   
+    myplotObj.setY(-4,8)
     
     ### not really options, the rest is functional
     # add the datastreams to the myplot object (or window/graph)
@@ -204,25 +229,32 @@ if __name__ == "__main__":
     myplotObj.addStream(pidstream)
     myplotObj.addStream(positionstream)
     
-    # set first goal, gains, setpoint, graph title
+    # set first goal
     goal = goallist[goalidx]
-    pidObj = PID(kplist[gainidx], kilist[gainidx], kdlist[gainidx], deadband=0, refreshTime=0.08, add=True, smooth=True)
+    # set first gains
+    pidObj = PID(
+        kplist[gainidx], kilist[gainidx], kdlist[gainidx],  # gains
+        deadband=0, refreshTime=0.08, add=True,             # options
+        rampUp=0.4, rampDown=0.3                            # ramping limits (time to full speed/ full stop)
+        )
+    # set first setpoint
     pidObj.setSP(goal)
+    # set first graph title
     myplotObj.setTitle('PID Example\n' + titlelist[gainidx])
     
     # enable the PID object (means it will calculate and return control values)
     pidObj.enable(pos[0])
     
-    # timing and counting
+    ## timing and counting
     runStart = time.time()
     goalStart = time.time()
     cycleCount = 0
     goalChanges = 0
     
     
-    ### this is where the fun happens
-    # myplot.loop() will clear its window each cycle and break the while
-    # when the window is closed
+    ## this is where the fun happens
+    # myplot.loop() will clear its window each cycle and break the
+    # while condition when the window is closed
     while myplotObj.loop():
         now = time.time()
         
@@ -246,19 +278,18 @@ if __name__ == "__main__":
             if np.mod(goalChanges,len(goallist)) == 0:
                 gainidx = np.mod(gainidx+1, len(kplist)) # increment and loop gain indexer
                 
-                # until we've done a complete cycle of all gain sets, 
+                # after we've done a complete cycle of all gain sets, 
                 # the PID gains are updated without enabling the PID,
                 # then enabled right after (just showing both uses)
                 if goalChanges < len(kplist):
                     # gains set, PID not enabled
-                    pidObj.setGains(kplist[gainidx], kilist[gainidx], kdlist[gainidx], 0)
+                    pidObj.setGains(kplist[gainidx], kilist[gainidx], kdlist[gainidx], enable=True)                    
+                else:
+                    # gains set and PID not enabled
+                    pidObj.setGains(kplist[gainidx], kilist[gainidx], kdlist[gainidx], enable=False)
                     # pidObj.setGains(kplist[gainidx], kilist[gainidx], kdlist[gainidx])
-                    
                     # enable calculation
                     pidObj.enable()
-                else:
-                    # gains set and PID enabled
-                    pidObj.setGains(kplist[gainidx], kilist[gainidx], kdlist[gainidx], 1)
                 
                 # update graph title
                 myplotObj.setTitle('PID Example\n' + titlelist[gainidx])
@@ -267,6 +298,9 @@ if __name__ == "__main__":
             if goalChanges >= (len(kplist) * len(goallist)):
                 gravity = 0.5
                 myplotObj.setTitle('PID Example with gravity\n' + titlelist[gainidx])
+        
+        # end stepping through goals and settings
+        
         
         # error, speed = obj.get(system's last position)
         E, S = pidObj.get(pos[-1])
